5265aaf460dffab5bbadb830865d16db4ff8d935
stevieing <stevieing@sky.com>
2016-02-11 11:43:39 +0000

    Changed seeds to make it work.

--- rails/db/seeds.rb
+++ rails/db/seeds.rb
@@ -6,14 +6,10 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
        
-LabelType.create(id: 2, name: "Tube", pitch_length: "0430", print_width: "0300", print_length: "0400", feed_value: "08", fine_adjustment: "022")
-LabelType.create(id: 3,name: "Rack",pitch_length: "0110",print_width: "0920",print_length: "0080",feed_value: "08",fine_adjustment: "004")
-
-plate = LabelType.create(name: "Plate", pitch_length: "0110", print_width: "0920", print_length: "0080", feed_value: "08", fine_adjustment: "004")
+plate = LabelType.create(name: "Plate", pitch_length: "0110", print_width: "0920", print_length: "0080", feed_value: "008", fine_adjustment: "04")
 
 header_text_1 = Bitmap.new(field_name: "header_text_1", x_origin: "0020", y_origin: "0035", horizontal_magnification: "1", vertical_magnification: "1", font: "G", space_adjustment: "00", rotational_angles: "00")
 header_text_2 = Bitmap.new(field_name: "header_text_2", x_origin: "0020", y_origin: "0065", horizontal_magnification: "1", vertical_magnification: "1", font: "G", space_adjustment: "00", rotational_angles: "00")
-
 
 location = Bitmap.new(field_name: "location", x_origin: "0030", y_origin: "0035", horizontal_magnification: "05", vertical_magnification: "1", font: "G", space_adjustment: "00", rotational_angles: "00")
 parent_location = Bitmap.new(field_name: "parent_location", x_origin: "0030", y_origin: "0065", horizontal_magnification: "05", vertical_magnification: "1", font: "G", space_adjustment: "00", rotational_angles: "00")
