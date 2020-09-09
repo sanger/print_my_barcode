26bfef329ccaff0a48c37249547ce3acf4239481
Ben Topping <ben.topping@outlook.com>
2020-09-09 15:38:52 +0100

    added echo to show printers rake task runnning

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,6 +3,8 @@ mkdir /etc/cups/
 echo "ServerName $CUPSD_SERVER_NAME" > /etc/cups/client.conf
 if [ $CUPSD_UPDATE == "true" ]
 then
+    echo "-> Running printers rake task"
     bundle exec rails printers:create
+    echo "-> Printers rake task complete"
 fi
 exec "$@"
