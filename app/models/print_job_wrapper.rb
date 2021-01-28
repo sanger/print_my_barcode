cbf577160812c50063054c88671e7e97d9282eb3
Harriet Craven <hc6@sanger.ac.uk>
2021-01-28 12:59:39 +0000

    feedback from code review

--- app/models/print_job_wrapper.rb
+++ app/models/print_job_wrapper.rb
@@ -7,66 +7,47 @@ class PrintJobWrapper
   attr_reader :copies
   attr_accessor :printer_name, :label_template_name, :label_template_id, :labels, :errors
 
-  # TODO: validate
-  # validate :check_attributes, :check_print_job
-
   def print
-    # return false unless valid?
+    return false unless print_job.valid?
 
+    print_job.execute
+    # do something with response
+  end
+
+  def print_job
     case printer.printer_type
     when 'toshiba'
-      print_to_toshiba
+      @print_job = toshiba_print_job
     when 'squix'
-      print_to_squix
+      @print_job = squix_print_job
     end
-    true
+  end
+
+  def toshiba_print_job
+    LabelPrinter::PrintJob.build(print_job_body)
+  end
+
+  def squix_print_job
+    Squix::PrintJob.new(print_job_body)
+  end
+
+  def print_job_body
+    case printer.printer_type
+    when 'toshiba'
+      unless label_template_id
+        @label_template_id = LabelTemplate.find_by(name: label_template_name).id
+      end
+      @print_job_body = { printer_name: printer_name, label_template_id: label_template_id,
+                          labels: labels }
+    when 'squix'
+      @print_job_body = { printer_name: printer_name, label_template_name: label_template_name,
+                          labels: labels, copies: copies }
+    end
   end
 
   def printer
     Printer.find_by(name: printer_name)
   end
-
-  def print_to_toshiba
-    unless label_template_id
-      @label_template_id = LabelTemplate.find_by(name: label_template_name).id
-    end
-
-    # labels here from LabWhere already includes the correct number of copies
-    body = { printer_name: printer_name, label_template_id: label_template_id, labels: labels }
-
-    print_job = LabelPrinter::PrintJob.build(body)
-    print_job.execute
-    # do something with response
-  end
-
-  def print_to_squix
-    squix_print_job = Squix::PrintJob.new(
-      printer_name: printer_name,
-      label_template_name:
-      label_template_name,
-      labels: labels,
-      copies: copies
-    )
-    squix_print_job.execute
-    # do something with response
-  end
-
-  def create_response
-    { message: '', status: '' }
-  end
-
-  # def check_attributes
-  #   errors.add(:printer_name, 'does not exist') if printer_name.nil?
-  #   errors.add(:label_template_name, 'does not exist') if label_template_name.nil?
-  # end
-
-  # def check_print_job
-  # case printer.printer_type
-  # when 'Toshiba'
-  #   toshiba.valid?
-  # when 'Squix'
-  #   toshiba.valid?
-  # end
 
   def copies=(copies)
     @copies = copies.to_i
--- spec/models/print_job_wrapper_spec.rb
+++ spec/models/print_job_wrapper_spec.rb
@@ -4,93 +4,143 @@ require 'rails_helper'
 
 RSpec.describe PrintJobWrapper do
 
-  let!(:printer)            { create(:printer) }
+  let!(:squix_printer)       { create(:printer, printer_type: :squix) }
+  let!(:toshiba_printer)     { create(:printer, printer_type: :toshiba) }
   let(:label_template)      { create(:label_template) }
   let(:labels)              { [{ 'label' => { 'test_attr' => 'test', 'barcode' => '12345' } }] }
-  let(:copies)              { 1 }
+  let(:copies)              { '1' }
 
   describe 'init' do
-    it 'has the correct insatnce variables' do
-      pjw = PrintJobWrapper.new({ printer_name: printer.name, label_template_name: label_template.name, label_template_id: label_template.id, labels: labels, copies: copies })
+    it 'has the correct instance variables' do
+      pjw = PrintJobWrapper.new({ printer_name: toshiba_printer.name, label_template_name: label_template.name, label_template_id: label_template.id, labels: labels, copies: copies })
 
-      assert_equal printer.name, pjw.printer_name
+      assert_equal toshiba_printer.name, pjw.printer_name
       assert_equal label_template.id, pjw.label_template_id
       assert_equal label_template.name, pjw.label_template_name
       assert_equal labels, pjw.labels
-      assert_equal copies, pjw.copies
+      assert_equal copies.to_i, pjw.copies
     end
   end
 
-  describe 'print' do
-    it 'calls print_to_toshiba when printer_type is Toshiba' do
-      toshiba_printer = create(:printer, printer_type: :toshiba)
-      pjw = PrintJobWrapper.new({ printer_name: toshiba_printer.name, label_template_name: label_template.name, label_template_id: label_template.id, labels: labels, copies: copies })
-      expect(pjw).to receive(:print_to_toshiba)
-      pjw.print
+  describe '#print_job' do
+    it 'sets the print job for a Toshiba printer' do
+      body = { printer_name: toshiba_printer.name, label_template_id: label_template.id, labels: labels }
+      pjw = PrintJobWrapper.new(body)
+      toshiba_print_job = LabelPrinter::PrintJob.build(body)
+
+      allow(pjw).to receive(:toshiba_print_job).and_return(toshiba_print_job)
+      expect(pjw.print_job).to eq pjw.toshiba_print_job
     end
 
-    it 'calls print_to_sprint when printer_type is Squix' do
-      squix_printer = create(:printer, printer_type: :squix)
-      pjw = PrintJobWrapper.new({ printer_name: squix_printer.name, label_template_name: label_template.name, label_template_id: label_template.id, labels: labels, copies: copies })
+    it 'sets the print job for a Squix printer' do
+      body = { printer_name: squix_printer.name, label_template_name: label_template.name, labels: labels, copies: copies }
+      pjw = PrintJobWrapper.new(body)
+      squix_print_job = Squix::PrintJob.new(body)
 
-      expect(pjw).to receive(:print_to_squix)
-      pjw.print
+      allow(pjw).to receive(:squix_print_job).and_return(squix_print_job)
+      expect(pjw.print_job).to eq pjw.squix_print_job
     end
   end
 
-  describe 'print_to_toshiba' do
-    describe 'when label_template id is not present' do
-      it 'builds a LabelPrinter::PrintJob with the correct arributes and executes' do
-        label_template = create(:label_template)
-        print_job = build(:print_job)
-        pjw = PrintJobWrapper.new({ printer_name: printer.name, label_template_name: label_template.name, label_template_id: nil, labels: labels, copies: copies })
+  describe '#print' do
+    context 'when printer_type is Toshiba' do
+      it 'validates the print job and calls execute when valid' do
+        body = { printer_name: toshiba_printer.name, label_template_id: label_template.id, labels: labels }
 
-        allow(LabelPrinter::PrintJob).to receive(:build).and_return(print_job)
+        pjw = PrintJobWrapper.new(body)
+        toshiba_print_job = LabelPrinter::PrintJob.build(body)
 
-        expected_body = { label_template_id: label_template.id, labels: labels, printer_name: printer.name }
-        expect(LabelPrinter::PrintJob).to receive(:build).with(expected_body)
-        expect(print_job).to receive(:execute)
-        pjw.print_to_toshiba
+        allow(pjw).to receive(:toshiba_print_job).and_return(toshiba_print_job)
+        allow(toshiba_print_job).to receive(:execute)
+
+        pjw.print
+        expect(toshiba_print_job.valid?).to eq true
+      end
+
+      it 'validates the print job and returns false when invalid' do
+        body_missing_attribute = { printer_name: toshiba_printer.name, labels: labels }
+
+        pjw = PrintJobWrapper.new(body_missing_attribute)
+        toshiba_print_job = LabelPrinter::PrintJob.build(body_missing_attribute)
+
+        allow(pjw).to receive(:toshiba_print_job).and_return(toshiba_print_job)
+        allow(toshiba_print_job).to receive(:execute)
+
+        pjw.print
+        expect(toshiba_print_job.valid?).to eq false
+        expect(toshiba_print_job.errors.messages).to eq({ label_template: ["does not exist"] })
       end
     end
 
-    describe 'when label_template id is present' do
-      it 'builds a LabelPrinter::PrintJob with the correct arributes' do
-        print_job = build(:print_job)
-        pjw = PrintJobWrapper.new({ printer_name: printer.name, label_template_name: nil, label_template_id: label_template.id, labels: labels, copies: copies })
+    context 'when printer_type is Squix' do
+      it 'validates the print job and calls execute when valid' do
+        body = { printer_name: squix_printer.name, label_template_name: label_template.name, labels: labels, copies: copies }
+        pjw = PrintJobWrapper.new(body)
+        squix_print_job = Squix::PrintJob.new(body)
 
-        allow(LabelPrinter::PrintJob).to receive(:build).and_return(print_job)
+        allow(pjw).to receive(:squix_print_job).and_return(squix_print_job)
+        allow(squix_print_job).to receive(:execute)
 
-        expected_body = { label_template_id: label_template.id, labels: labels, printer_name: printer.name }
-        expect(LabelPrinter::PrintJob).to receive(:build).with(expected_body)
-        expect(print_job).to receive(:execute)
+        pjw.print
+        expect(squix_print_job.valid?).to eq true
+      end
 
-        pjw.print_to_toshiba
+      it 'validates the print job and returns false when invalid' do
+        body_missing_attribute = { printer_name: squix_printer.name, labels: labels, copies: copies }
+        pjw = PrintJobWrapper.new(body_missing_attribute)
+        squix_print_job = Squix::PrintJob.new(body_missing_attribute)
+
+        allow(pjw).to receive(:squix_print_job).and_return(squix_print_job)
+        allow(squix_print_job).to receive(:execute)
+
+        pjw.print
+        expect(squix_print_job.valid?).to eq false
+        expect(squix_print_job.errors.messages).to eq({ label_template_name: ["can't be blank"] })
       end
     end
   end
 
-  describe 'print_to_squix' do
-    it 'builds a Squix::PrintJob with the correct arributes' do
-      squix_print_job = build(:squix_print_job)
-      pjw = PrintJobWrapper.new({ printer_name: printer.name, label_template_name: label_template.name, label_template_id: label_template.id, labels: labels, copies: copies })
+  describe '#toshiba_print_job' do
+    it 'builds a LabelPrinter::PrintJob when label_template_id is provided' do
+      body = { printer_name: toshiba_printer.name, label_template_id: label_template.id, labels: labels }
+      pjw = PrintJobWrapper.new(body)
+      expect(pjw.toshiba_print_job).to be_instance_of(LabelPrinter::PrintJob::LPD)
+    end
 
-      allow(Squix::PrintJob).to receive(:new).and_return(squix_print_job)
-
-      expected_body = { printer_name: printer.name, label_template_name: label_template.name, labels: labels, copies: copies }
-      expect(Squix::PrintJob).to receive(:new).with(expected_body)
-      expect(squix_print_job).to receive(:execute)
-
-      pjw.print_to_squix
+    it 'builds a LabelPrinter::PrintJob when label_template_name is provided' do
+      body = { printer_name: toshiba_printer.name, label_template_name: label_template.name, labels: labels }
+      pjw = PrintJobWrapper.new(body)
+      expect(pjw.toshiba_print_job).to be_instance_of(LabelPrinter::PrintJob::LPD)
     end
   end
 
-  describe 'create_response' do
+  describe '#squix_print_job' do
+    it 'builds a Squix::PrintJob' do
+      body = { printer_name: squix_printer.name, label_template_name: label_template.name, label_template_id: label_template.id, labels: labels, copies: copies }
+      pjw = PrintJobWrapper.new(body)
+      expect(pjw.squix_print_job).to be_instance_of(Squix::PrintJob)
+    end
   end
 
-  describe 'get_printer_type' do
-    # TODO GPL-831-2
-    it 'returns the correct printer type' do
+  describe '#print_job_body' do
+    it 'sets the print_job_body for a Toshiba printer' do
+      body = { printer_name: toshiba_printer.name, label_template_name: label_template.name, labels: labels }
+      pjw = PrintJobWrapper.new(body)
+      expect(pjw.print_job_body.keys).to eq [:printer_name, :label_template_id, :labels]
+    end
+
+    it 'sets the print_job_body for a Squix printer' do
+      body = { printer_name: squix_printer.name, label_template_name: label_template.name, label_template_id: label_template.id, labels: labels, copies: copies }
+      pjw = PrintJobWrapper.new(body)
+      expect(pjw.print_job_body.keys).to eq [:printer_name, :label_template_name, :labels, :copies]
+    end
+  end
+
+  describe '#printer' do
+    it 'returns the correct printer' do
+      body = { printer_name: squix_printer.name, label_template_name: label_template.name, label_template_id: label_template.id, labels: labels, copies: copies }
+      pjw = PrintJobWrapper.new(body)
+      expect(pjw.printer).to eq squix_printer
     end
   end
 end
