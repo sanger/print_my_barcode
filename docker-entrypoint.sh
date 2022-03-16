a0c365a58f170e7f82b12e8ee7971ba09bf6b3df
Ben Topping <ben.topping@outlook.com>
2022-03-16 13:38:05 +0000

    added cups waiting to docker entrypoint so create:printers rake task doesnt error

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 mkdir /etc/cups/
 echo "ServerName $CUPSD_SERVER_NAME" > /etc/cups/client.conf
+TIMEOUT=120
+CUPSPORT=631
+CUPSHOST=cupsd
 if [ $CUPSD_UPDATE == "true" ]
 then
-    echo "-> Printing printer names here:"
-    bundle exec rails runner "puts Printer.all.map(&:name)"
+    echp "-> Waiting for cupsd to become available"
+    ./wait_for_connection.sh "${CUPSHOST}" "${CUPSPORT}" "${TIMEOUT}"
     echo "-> Running printers rake task"
     bundle exec rails printers:create
     echo "-> Printers rake task complete"
