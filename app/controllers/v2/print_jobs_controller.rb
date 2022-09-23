8b4add03ab07e73c055f57213942d1315015ba54
Sangeetha <sb67@sanger.ac.uk>
2022-09-23 14:20:10 +0100

    Changed log level for testing

--- app/controllers/v2/print_jobs_controller.rb
+++ app/controllers/v2/print_jobs_controller.rb
@@ -3,7 +3,12 @@
 module V2
   # PrintJobsController
   class PrintJobsController < ApplicationController
+    # rubocop:disable Metrics/AbcSize
     def create
+      Rails.logger.info { 'IN PrintJobsController' }
+      Rails.logger.info { "Rails.logger.level #{Rails.logger.level}" }
+      Rails.logger.level = 0
+      Rails.logger.info { "Rails.logger.level #{Rails.logger.level}" }
       print_job_wrapper = PrintJobWrapper.new(print_job_params)
 
       if print_job_wrapper.print
@@ -12,17 +17,24 @@ module V2
         render_error print_job_wrapper
       end
     end
+    # rubocop:enable Metrics/AbcSize
 
     private
 
+    # rubocop:disable Metrics/AbcSize
     def print_job_params
       params.require(:print_job).permit(:printer_name, :label_template_name, :copies)
             .tap do |allow_listed|
         allow_listed[:labels] = []
         params[:print_job][:labels].each do |label|
+          Rails.logger.level = 0
+          Rails.logger.debug { "LIST #{allow_listed}" }
+          Rails.logger.debug { "LABEL #{label}" }
+          Rails.logger.debug { "LABEL PERMIT #{label.permit}" }
           allow_listed[:labels] << label.permit!
         end
       end
     end
+    # rubocop:enable Metrics/AbcSize
   end
 end
