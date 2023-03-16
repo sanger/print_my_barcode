842234a84176fc1fa8a32fb534c14a37f1cc0836
Sangeetha <sb67@sanger.ac.uk>
2023-03-16 09:48:14 +0000

    Testing the build failure issue

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -2,6 +2,7 @@
 
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV['RAILS_ENV'] ||= 'test'
+require 'factory_bot_rails'
 require 'spec_helper'
 require File.expand_path('../config/environment', __dir__)
 require 'rspec/rails'
