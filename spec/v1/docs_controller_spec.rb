fdcd1bdc2841c34e8d40723f6f0b4911f28ba1c5
Stephen Hulme <sh51@sanger.ac.uk>
2024-05-28 14:33:22 +0100

    tests: rename documentation tests

--- spec/v1/docs_controller_spec.rb
+++ spec/v1/docs_controller_spec.rb
@@ -4,10 +4,10 @@ require 'rails_helper'
 
 RSpec.describe V1::DocsController, type: :request do
 
-  it 'shows me some crazy data' do
+  it 'should render the documentation' do
     get v1_docs_path
     expect(response).to be_successful
-    expect(response.body).to include('Print My Barcode')
+    expect(response.body).to include('Print My Barcode (API V1)')
   end
-  
+
 end
--- spec/v2/docs_controller_spec.rb
+++ spec/v2/docs_controller_spec.rb
@@ -4,10 +4,10 @@ require 'rails_helper'
 
 RSpec.describe V2::DocsController, type: :request do
 
-  it 'shows me some crazy data' do
+  it 'should render the documentation' do
     get v2_docs_path
     expect(response).to be_successful
-    expect(response.body).to include('Print My Barcode')
+    expect(response.body).to include('Print My Barcode (API V2)')
   end
 
 end
