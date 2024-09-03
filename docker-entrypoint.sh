5937058278f42bd36c1ada1353c2eed7c07a003d
Ben Topping <ben.topping@outlook.com>
2024-09-03 14:48:14 +0100

    fix(cups): updates wait for script to be cups-scheduler specific

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,13 +2,11 @@
 mkdir /etc/cups/
 echo "ServerName $CUPSD_SERVER_NAME" > /etc/cups/client.conf
 TIMEOUT=120
-CUPSPORT=631
-CUPSHOST=cupsd
 
 if [[ $CUPSD_UPDATE == "true" ]]
 then
     echo "-> Waiting for cups to become available"
-    bash ./wait_for_connection.sh "${CUPSHOST}" "${CUPSPORT}" "${TIMEOUT}"
+    bash ./wait_for_cups.sh "${TIMEOUT}"
     echo "-> Running printers rake task"
     bundle exec rails printers:create
     echo "-> Printers rake task complete"
--- wait_for_cups.sh
+++ wait_for_cups.sh
@@ -1,23 +1,20 @@
 #!/usr/bin/env bash
 
 #
-# This script waits TIMEOUT seconds for connection to HOST:PORT
-# to be stablished and exit with 0 if success or 1 if error
+# This script waits TIMEOUT seconds for connection to cups
+# to be established and exit with 0 if success or 1 if error
 set -o pipefail
 set -o nounset
 
-HOST=$1
-PORT=$2
-TIMEOUT=$3
+TIMEOUT=$1
 
 TIMEOUT_END=$(($(date +%s) + TIMEOUT))
 result=1
 while [ $result -ne 0 ]; do
-  echo "Waiting for connection to ${HOST}:${PORT}..."
-  wget --spider "${HOST}:${PORT}"
-  result=$?
+  echo "Waiting for connection to cups..."
+  result="`lpstat -r | grep 'scheduler is not running' | wc -l`"
   if [ $result -eq 0 ]; then
-    echo "Connected to ${HOST}:${PORT}."
+    echo "Connected to cups"
     exit 0
   else
     if [ $(date +%s) -ge $TIMEOUT_END ]; then
